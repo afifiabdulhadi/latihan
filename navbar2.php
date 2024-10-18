a41090dacd2bec9adaa18020c00c9e128bbb8da8
wikiramadan <wikiramadan8@gmail.com>
2024-10-18 16:10:28 +0700

    edit.nav

--- navbar2.php
+++ navbar2.php
@@ -222,6 +222,91 @@
             </div>
         </div>
     </div>
+    
+    <h3 class="fst-italic display-3 text-center">Berita</h3>
+    <div class="container md-5">
+    <div class="row">
+
+      <!-- Berita 1 -->
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="gambar/banjir.jpg" class="card-img-top news-img rounded mx-auto d-block" alt="Gambar Banjir">
+          <div class="card-body">
+            <h5 class="news-title">Banjir Melanda Pangkalpinang</h5>
+            <p class="news-body" style="width: 350px;">Hujan deras selama tiga hari berturut-turut menyebabkan banjir besar di daerah Pangkalpinang...</p>
+            <a href="detailberita1.php" class="btn btn-primary">Baca Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Berita 2 -->
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="gambar/dpr.jpeg" class="card-img-top news-img rounded mx-auto d-block" alt="Gambar DPR">
+          <div class="card-body">
+            <h5 class="news-title">Anggota DPR Tertangkap Korupsi</h5>
+            <p class="news-body"  style="width: 300px;">Seorang anggota DPR ditangkap oleh KPK atas dugaan kasus korupsi proyek infrastruktur...</p>
+            <a href="detailberita2.php" class="btn btn-primary">Baca Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Berita 3 -->
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="gambar/kec.jpeg" class="card-img-top news-img rounded mx-auto d-block " alt="Gambar Kecelakaan">
+          <div class="card-body">
+            <h5 class="news-title">Kecelakaan di Jalan Tol</h5>
+            <p class="news-body" style="width: 310px;">Sebuah kecelakaan melibatkan mobil pribadi di jalan tol menyebabkan arus lalu lintas tersendat...</p>
+            <a href="detailberita3.php" class="btn btn-primary">Baca Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="row">
+
+      <!-- Berita 4 -->
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="gambar/prabowo.jpg" class="card-img-top news-img rounded mx-auto d-block" alt="Gambar Prabowo">
+          <div class="card-body">
+            <h5 class="news-title">Prabowo dalam Perjalanan Politik</h5>
+            <p class="news-body"  style="width: 300px;">Prabowo Subianto kembali mencalonkan diri sebagai presiden dengan dukungan dari berbagai partai...</p>
+            <a href="detailberita4.php" class="btn btn-primary">Baca Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Berita 5 -->
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="gambar/kebakaranhutan.jpg" class="card-img-top news-img rounded mx-auto d-block" alt="Gambar Kebakaran Hutan">
+          <div class="card-body">
+            <h5 class="news-title">Kebakaran Hutan Meluas</h5>
+            <p class="news-body" style="width: 300px;">Kebakaran hutan yang terjadi di wilayah Sumatera semakin meluas dan mengakibatkan kerusakan lingkungan...</p>
+            <a href="detailberita5.php" class="btn btn-primary">Baca Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+      <!-- Berita 6 -->
+      <div class="col-md-4 mb-4">
+        <div class="card">
+          <img src="gambar/tauran.jpg" class="card-img-top news-img rounded mx-auto d-block" alt="Gambar Tauran">
+          <div class="card-body">
+            <h5 class="news-title">Tauran di Jakarta</h5>
+            <p class="news-body"  style="width: 300px;">Sejumlah remaja tertangkap usai terlibat dalam aksi tawuran dngan kelopok lain di pusat kota Jakarta...</p>
+            <a href="detailberita6.php" class="btn btn-primary">Baca Selengkapnya</a>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
+
+
 
     <footer>
         <p> 2024 lubuksikaping. kanawk.</p>
