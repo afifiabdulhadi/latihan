d8ac2f5f7b1c62a89e6d1f71e04610fdab1e3cab
afifiabdulhadi <afifiabdulhadi0.gmail.com>
2024-10-24 09:59:51 +0700

    edit.login

--- pages/login.php
+++ pages/login.php
@@ -3,36 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Form</title>
+    <title>Document</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../styles.css">
+
 </head>
 <body>
-
-<div class="container mt-5">
-    <div class="row justify-content-center">
-        <div class="col-12 col-md-6 login-container">
-            <h2 class="text-center bg-">Login</h2>
-            <form action="login.php" method="POST">
-                <div class="form-group">
-                    <label for="username">Username:</label>
-                    <input type="text" class="form-control" name="username" id="username" required>
+    <div class="container mt-5">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="text-center">Login</h3>
+                        <form action="login.php" method="POST">
+                            <div class="form-group">
+                                <label for="username">Username:</label>
+                                <input type="text" class="form-control" id="username" name="username" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password:</label>
+                                <input type="password" class="form-control" id="password" name="password" required>
+                            </div>
+                            <button type="submit" class="btn btn-primary btn-block">Login</button>
+                            <a href="lupasandi.php" class="d-block text-center mt-2">Lupa Kata Sandi?</a>
+                        </form>
+                    </div>
                 </div>
-                <div class="form-group">
-                    <label for="password">Password:</label>
-                    <input type="password" class="form-control" name="password" id="password" required>
-                </div>
-                <a href="lupasandi.php">lupakatasandi</a>
-                <p type="submit" class="btn btn-register btn-block"><a href="reqister.php">Login</></a></p>
-            </form>
+            </div>
         </div>
     </div>
-</div>
-
-<!-- Bootstrap JS (optional) -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-
+    
+    
 </body>
 </html>
